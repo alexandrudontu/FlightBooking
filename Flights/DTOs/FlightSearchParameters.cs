da2e2d24a50190a5b2ad8e4035296e891ffe3ffa
alexandrudontu <alexandrudontu@users.noreply.github.com>
2024-08-09 10:38:02 +0300

    Add project files.

---
+++ Flights/DTOs/FlightSearchParameters.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace Flights.DTOs
+{
+    public record FlightSearchParameters(
+
+        [DefaultValue("07/05/2024 11:30:00 AM")]
+        DateTime? FromDate,
+
+        [DefaultValue("07/06/2024 11:30:00 AM")]
+        DateTime? ToDate,
+
+        [DefaultValue("Los Angeles")]
+        string? From,
+
+        [DefaultValue("Berlin")]
+        string? Destination,
+
+        [DefaultValue(1)]
+        int? NumberOfPassengers
+        );
+
+}
